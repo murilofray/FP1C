cb8876a99361b57e6090feab31e0ba3e7ae609be
MuriloFray <murilofray01@gmail.com>
2022-08-19 15:41:23 -0300

    at2

--- src/TesteProdutoCategoria.java
+++ src/TesteProdutoCategoria.java
@@ -1,4 +1,3 @@
-
 import br.edu.ifsp.pep.modelo.Categoria;
 import br.edu.ifsp.pep.modelo.Produto;
 import br.ifsp.edu.pep.dao.CategoriaDAO;
@@ -14,39 +13,79 @@ import javax.persistence.TypedQuery;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Aluno
  */
 public class TesteProdutoCategoria {
+
+    private static ProdutoDAO produtoDAO = new ProdutoDAO();
+
     public static void main(String[] args) {
-            EntityManager em = Persistence
-                .createEntityManagerFactory("aula1PU")
-                .createEntityManager();
-        
-        Categoria categoria = new Categoria ();
-        categoria.setDescricao("Informática");
-        
+        adicionarProduto();
+        exibirProdutos();
+       // reajustarPreco(10);
+    }
+
+    private static void teste() {
+//        EntityManager em = Persistence
+//                .createEntityManagerFactory("aula1PU")
+//                .createEntityManager();
+//
+//        Categoria categoria = new Categoria();
+//        categoria.setDescricao("Informática");
+//
+//        CategoriaDAO categoriaDAO = new CategoriaDAO();
+//        categoriaDAO.inserir(categoria);
+//
+//        Produto produto = new Produto();
+//        produto.setDescricao("Teclado");
+//        produto.setPreco(new BigDecimal(60.0));
+//        produto.setQuantidade(100);
+//
+//        produto.setCategoria(categoria);
+//
+//        produtoDAO.inserir(produto);
+//
+//        List<Produto> produtos = produtoDAO.buscar();
+//        for (Produto p : produtos) {
+//            System.out.println("Descrição: " + p.getDescricao());
+//            System.out.println("Categoria: " + p.getCategoria().getDescricao());
+//        }
+    }
+
+    private static void adicionarProduto() {
         CategoriaDAO categoriaDAO = new CategoriaDAO();
-        categoriaDAO.inserir(categoria);
-        
-        Produto produto = new Produto();
-        produto.setDescricao("Teclado");
-        produto.setPreco(new BigDecimal(60.0));
-        produto.setQuantidade(100);
-        
-        produto.setCategoria(categoria);
-        
-        ProdutoDAO produtoDao = new ProdutoDAO();
-        produtoDao.inserir(produto);
-        
-        List<Produto> produtos = produtoDao.buscar();
-        for (Produto p : produtos) {
+
+        for (int i = 0; i < 10; i++) {
+            Categoria categoria = new Categoria();
+            categoria.setDescricao("Categoria " + i);
+            categoriaDAO.inserir(categoria);
+
+            Produto produto = new Produto();
+            produto.setDescricao("Produto " + i);
+            produto.setPreco(new BigDecimal(60.0 * (i + 1)));
+            produto.setQuantidade(100);
+
+            produto.setCategoria(categoria);
+
+            produtoDAO.inserir(produto);
+        }
+    }
+
+    private static void exibirProdutos() {
+        for (Produto p : produtoDAO.buscar()) {
             System.out.println("Descrição: " + p.getDescricao());
             System.out.println("Categoria: " + p.getCategoria().getDescricao());
         }
- 
     }
     
+//     private static void reajustarPreco(double porcentagem) {
+//         for (Produto p : produtoDAO.buscar()) {
+//             BigDecimal ajustar = (p.getPreco().multiply((porcentagem/100)));
+//             p.setPreco(ajustar.add(p.getPreco()));
+//             
+//             produtoDAO.alterar(p);
+//         }
+//     }
 }
